ee3a0f3c87e11bfe27de18e70e0949e1a9202112
Ram Sujan Rajak <ramsujanrjak2001@gmail.com>
2024-06-28 23:01:53 +0530

    add the footer component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import { TodoProvider } from "./contexts/TodoContext";
 import TodoItem from "./components/TodoItem";
 import TodoForm from "./components/TodoForm";
+import Footer from "./components/Footer";
 
 function App() {
   const [todos, setTodos] = useState([]);
@@ -48,33 +49,36 @@ function App() {
     <TodoProvider
       value={{ todos, addTodo, updatedTodo, deleteTodo, toggleComplete }}
     >
-      <div className="bg-[#172842] min-h-screen py-8">
-        <div
-          className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white"
-          style={{
-            backgroundColor: "#fff",
-            padding: "20px",
-            borderRadius: "8px",
-            color: "black",
-            boxShadow: "0 4px 50px rgba(255, 255, 255, .5)",
-          }}
-        >
-          <h1 className="text-2xl font-bold text-center mb-8 mt-2">
-            Manage Your Todos
-          </h1>
-          <div className="mb-4">
-            {/* Todo form goes here */}
-            <TodoForm />
-          </div>
-          <div className="flex flex-wrap gap-y-3">
-            {/*Loop and Add TodoItem here */}
-            {todos.map((todo) => (
-              <div key={todo.id} className="w-full">
-                <TodoItem todo={todo} />
-              </div>
-            ))}
+      <div className="bg-[#172842] flex flex-col min-h-screen">
+        <div className="flex-grow">
+          <div
+            className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-black"
+            style={{
+              backgroundColor: "#fff",
+              padding: "20px",
+              borderRadius: "8px",
+              marginTop:"30px",
+              boxShadow: "0 4px 50px rgba(255, 255, 255, .5)",
+            }}
+          >
+            <h1 className="text-2xl font-bold text-center mb-8 mt-2">
+              Manage Your Todos
+            </h1>
+            <div className="mb-4">
+              {/* Todo form goes here */}
+              <TodoForm />
+            </div>
+            <div className="flex flex-wrap gap-y-3">
+              {/* Loop and Add TodoItem here */}
+              {todos.map((todo) => (
+                <div key={todo.id} className="w-full">
+                  <TodoItem todo={todo} />
+                </div>
+              ))}
+            </div>
           </div>
         </div>
+        <Footer />
       </div>
     </TodoProvider>
   );
